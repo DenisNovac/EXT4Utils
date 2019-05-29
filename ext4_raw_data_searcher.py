8671f84224eb00023251dd0ffad4a750735f172e
DenisNovac <ponydnk@gmail.com>
2019-05-29 23:35:42 +0500

    working search of file blocks

--- ext4_raw_data_searcher.py
+++ ext4_raw_data_searcher.py
@@ -8,8 +8,8 @@
 
 # Documentation https://ext4.wiki.kernel.org/index.php/Ext4_Disk_Layout#The_Contents_of_inode.i_block
 
-
 import sys
+import subprocess
 import struct
 
 # EXT4Utils DenisNovac repository dependencies:
@@ -29,7 +29,7 @@ GROUP_DESCRIPTOR_OFFSET=None
 BLOCKS_PER_GROUP=None
 # output of the program
 DATA_BLOCKS= [ ]
-
+INODE_DATA_BLOCKS_LENGTH=None
 
 
 # copy of function from ext4_raw_inode_searcher
@@ -82,6 +82,7 @@ def call_inode_searcher():
 
 
 def find_extent_tree():
+    global INODE_DATA_BLOCKS_LENGTH
     s_creator_os=RAW_SUPERBLOCK[0x48:0x48+4]
     if not toNumber(s_creator_os)==0:
         print("This program is only for Linux (s_creator_os=0). Terminating.")
@@ -91,7 +92,9 @@ def find_extent_tree():
     zeroes="\x00\x00"
     i_blocks=zeroes+i_blocks_hi[::-1]+i_blocks_lo[::-1]
     # this variable is 512-byte blocks on the disk, not actual 4096-byte blocks!!!
+    
     i_blocks_amount=struct.unpack(">q",i_blocks)[0]*512/BLOCK_SIZE
+    INODE_DATA_BLOCKS_LENGTH=i_blocks_amount
     print("Blocks of the file: "+str(i_blocks_amount))
 
     i_block_table_offset=0x28
@@ -110,17 +113,18 @@ def find_extent_tree():
         exit (-1)
     
     extent_tree_list= [ ]
-    print("\nExtent tree entries:")
+
     for i in range(0,len(extent_tree),12):
         extent_tree_list.append(extent_tree[i:i+12])
 
-    process_extent_tree( extent_tree_list )
+    process_inode_extent_tree( extent_tree_list )
     return None
 
 
 
-def process_extent_tree ( extent_tree_list ):
-    for i in range(0,5):
+def process_inode_extent_tree ( extent_tree_list ):
+    print("\nExtent tree entries:")
+    for i in range(0,len(extent_tree_list)):
         print("["+str(i)+"] "+extent_tree_list[i].encode('hex'))
 
     print("\nExtent header (Entry 0):")
@@ -144,7 +148,8 @@ def process_extent_tree ( extent_tree_list ):
             process_internal_node(extent_tree_list[i])
             processed=processed+1
     print("\n")
-    while not processed==5:
+
+    while not processed==eh_entries+1:
         if extent_tree_list[processed]=="\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00": break
         print("Entry "+str(processed)+":")
         leaf_node_blocks(extent_tree_list[processed])
@@ -156,7 +161,59 @@ def process_extent_tree ( extent_tree_list ):
 
 # entry is a valid 12-bytes struct ext4_extent_idx (internal node)
 def process_internal_node ( internal_node_entry ):
+    global DATA_BLOCKS
     print("\t"+internal_node_entry.encode('hex'))
+    ei_block=internal_node_entry[0x0:4]
+    ei_leaf_lo=internal_node_entry[0x4:0x4+4]
+    ei_leaf_hi=internal_node_entry[0x8:0x8+2]
+    ei_unused=internal_node_entry[0xA:0xA+2]
+
+    print("\tei_block (this index node covers file blocks from 'block' onward): "+str(toNumber(ei_block)))
+    zeroes="\x00\x00"
+    ei_leaf_bytes=zeroes+ei_leaf_hi[::-1]+ei_leaf_lo[::-1]
+    ei_leaf=struct.unpack(">q",ei_leaf_bytes)[0]
+    print("\tei_leaf (block number of extent node that is next level lower in the tree): "+str(ei_leaf))
+    DATA_BLOCKS.append(ei_leaf)
+    print("\tei_unused: "+ei_unused.encode('hex'))
+
+    path_to_device=sys.argv[2]
+    file=open(path_to_device,"r")
+    sb_offset=ei_leaf*BLOCK_SIZE
+    file.seek(sb_offset)
+    extent_node=file.read(12)
+    file.close()
+    print("\n\tExtent node: ")
+    print("\t"+extent_node.encode('hex'))
+    magic_num=extent_node[0:2]
+
+    if magic_num.encode('hex')=="0af3":
+        print("\tMagic number correct: 0xF30A. Working with Extent tree.")
+    else:
+        print("\tMagic number (0xF30A) of external node not correct.\nTerminated.")
+        exit (-1)
+
+    eh_entries=toNumber( extent_node[0x2:0x2+2] )
+    print("\tValid entries following the header: "+ str(eh_entries))
+    eh_max=toNumber( extent_node[0x4:0x4+2] )
+    print("\tMaximum valid entries following the header: "+ str(eh_max))
+    eh_depth=toNumber( extent_node [0x6:0x6+2] )
+    print("\tExtent depth is: "+str(eh_depth)+".\n")
+
+    file=open(path_to_device,"r")
+    sb_offset=ei_leaf*BLOCK_SIZE
+    file.seek(sb_offset)
+    extent_node=file.read(12+12*eh_entries)
+    file.close()
+
+    extent_node_list= []
+    for i in range(0,len(extent_node),12):
+        extent_node_list.append(extent_node[i:i+12])
+    #print("\tExtent node entries: ")
+    #for i in range(0,len(extent_node)/12):
+        #print("\t["+str(i)+"] "+extent_node_list[i].encode('hex'))
+    process_inode_extent_tree(extent_node_list)
+
+    return None
 
 
 
@@ -192,13 +249,58 @@ def leaf_node_blocks( entry ):
 
 
 
+# function to verify program output and debugfs output
+def verify_results():
+    print("VERIFYING RESULTS...")
+
+    bashCommand = "mount | grep "+sys.argv[2]
+    root_folder=subprocess.check_output(bashCommand,shell=True)
+    root_folder=root_folder.split("on",1)
+    root_folder=root_folder[1]
+    root_folder=root_folder.split("type",1)
+    root_folder=root_folder[0].strip()
+    
+    path=sys.argv[1]
+    if root_folder in path:
+        path=path.split(root_folder)[1]
+
+    bashCommand = "sudo debugfs "+sys.argv[2]+" -R 'blocks "+path+ "'"
+    output=subprocess.check_output(bashCommand,shell=True)
+    output=output.split(" ")
+
+    debugfs_data = []
+    try:
+        for i in output:
+            debugfs_data.append(int(i))
+    except ValueError:
+        pass
+    
+    # there is no duplicates in deubgfs output!
+    debugfs_data=list(set(debugfs_data))
+
+    # removing duplicates
+    program_data=list(set(DATA_BLOCKS))
+
+    print("debugfs file blocks: "+str(len(debugfs_data)))
+    print("program file blocks: "+str(len(program_data)))
+    if not len(debugfs_data)==len(program_data)==INODE_DATA_BLOCKS_LENGTH:
+        print("LENGTH ARE NOT EQUAL! VERIFYING TERMINATED")
+    
+    debugfs_data.sort()
+    program_data.sort()
+    print("Comparing lists of blocks...")
+    print(debugfs_data==program_data)
+    
+    return None
+
+
+
 def main():
     call_inode_searcher()
     print ("Global inode offset: "+str(INODE_OFFSET))
     print ("Global group number: "+str(GROUP_NUMBER))
     print ("Global group descriptor offset: "+str(GROUP_DESCRIPTOR_OFFSET))
     find_extent_tree()
-
-
+    verify_results()
 
 main()
